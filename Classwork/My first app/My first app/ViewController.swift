ab3dbb03f66ff99d84ecd66c3a71a53c39a3306a
Johanna Ostrich <jhostrich@gmail.com>
2014-12-17 21:13:15 -0500

    Created My first app files in Xcode

---
+++ Classwork/My first app/My first app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My first app
+//
+//  Created by Johanna Ostrich on 12/17/14.
+//  Copyright (c) 2014 Johanna Ostrich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
