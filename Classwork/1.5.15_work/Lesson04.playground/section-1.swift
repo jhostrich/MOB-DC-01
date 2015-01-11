c80f685bd91c5277e034ec2abcb3f3a98c4b0f29
Johanna Ostrich <jhostrich@gmail.com>
2015-01-10 20:57:19 -0500

    Classwork 1.7.15

--- Classwork/1.5.15_work/Lesson04.playground/section-1.swift
+++ Classwork/1.5.15_work/Lesson04.playground/section-1.swift
@@ -8,7 +8,8 @@ var myAge = 30
 
 
 // TODO: Print "Hello {whatever the value of name is}, you are {whatever the value of age is} years old!"
-println("Hello \(myName), you are \(myAge) years old!")
+println("Hello \(myName), you are \(myAge) years old!") //Interpolation
+"This is " + "concatenation"
 
 
 // TODO: Print “You can drink” below the above text if the user is above 21. If they are above 18, print “you can vote”. If they are above 16, print “You can drive”
@@ -22,7 +23,7 @@ if myAge > 21 {
 
 
 // TODO: Print “you can drive” if the user is above 16 but below 18. It should print “You can drive and vote” if the user is above 18 but below 21. If the user is above 21, it should print “you can drive, vote and drink (but not at the same time!”.
-if myAge > 16 && myAge < 18{
+if myAge > 16 && myAge < 18 {
     println("You can drive")
 } else if myAge > 18 && myAge < 21 {
     println("You can drive and vote")
@@ -37,13 +38,29 @@ for h in 1...50 {
     println(h * 7 - 1)
 }
 
+/* Scoping 
+
+for i in 1...50 {
+    var multiple = 7 * i - 1
+    println(multiple)
+}
+
+multiple is only valid inside the brackets here
+
+*/
+
+for var i = 0; i < 50; ++i {
+    println(7*i-1)
+}
+
+
 
 // TODO: Create a constant called number
 let number = 3
 
 
 // TODO: Print whether the above number is even
-if number % 2 == 0 {
+if number % 2 == 0 && number != 0 {
     println("It's even")
 } else if number % 2 == 1 {
     println("It's odd")
@@ -51,19 +68,28 @@ if number % 2 == 0 {
 
 
 // TODO: The first fibonacci number is 0, the second is 1, the third is 1, the fourth is two, the fifth is 3, the sixth is 5, etc. The Xth fibonacci number is the sum of the X-1th fibonacci number and the X-2th fibonacci number. Print the 37th fibonacci number below
-/*
-0, 1, 1, 2, 3, 5
 
-var x = 0
-var y = 1
-var z = x + y
+var x1 = 0, x2 = 1
+//note can define multiple variables at once like this--shorthand!
 
-for g = 37 {
-    println()
+//anyway:
+var fibNum = 37, current = 0, next = 1, result = 0
+for index in 0..<fibNum {
+    //current val is 3
+    //temp val becomes 3
+    let tempVar = current
+    //current value becomes next (5)
+    current = next
+    //next value becomes old current (temporary) + new current (old next)
+    next = tempVar + current
+    result = tempVar
 }
+println("Fib num is \(result)")
 
 
-*/
+// for For Loops, have to declare variable in advance
+// for For In Loops, don't have to declare variable, since it's a temporary variable
+// constants are technically more memory-light than variables, though it doesn't make a huge difference
 
 
 // TODO: Print out "Hello {whatever the value of name is}, your name is {however long the string name is} characters long!. Use countElements()
@@ -71,13 +97,48 @@ println("Hello \(myName), your name is \(countElements(myName)) characters long!
 
 
 // TODO: Print the sum of one hundred random numbers. Use rand() to generate random numbers.
+var sum = 0
+for i in 1...100 {
+    sum += Int(rand())
+    //here, we typecast it as an integer
+}
+println(sum)
+
+//var sum = 20
+//sum = sum + 10
+//sum += 10 means the same thing
+
+
+println(sum)
+var tempString = "10"
+tempString.toInt()!
+
+
+var sumB = 0
+for i in 1...100 {
+    sumB += String(rand()).toInt()!
+}
+println(sum)
+//I think the issue here is rand() is for something other than integers, so we have to make it an integer before it will work
 
 
 // Bonus TO DO: Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.
-var w: Int
-for w in 1...100 {
-    println(w)
-}
+let fizz = 3
+let buzz = 5
 
+for var i = 1; i <= 100; ++i {
+    if ((i % fizz) == 0 && (i % buzz) == 0) {
+        println("FizzBuzz")
+    }
+    else if ((i % fizz) == 0) {
+        println("Fizz")
+    }
+    else if ((i % buzz) == 0) {
+        println("Buzz")
+    }
+    else {
+        println("Number \(i)")
+    }
+}
 
 //tuple is an array with multiple types
---
+++ Classwork/1.7.15_work/functionsSample.playground/section-1.swift
@@ -0,0 +1,144 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+func hello() {
+    println("Hello world")
+}
+
+hello()
+
+//parameter is a temporary variable, it's only available within the function
+
+func sum(firstNum: Int, secondNum: Int) {
+    firstNum + secondNum
+    secondNum - firstNum
+}
+
+sum (10, 10)
+
+// "-> Int" is saying want integer result
+func sumB(firstNum: Int, secondNum: Int) -> Int {
+    let newSum = firstNum + secondNum
+    return newSum
+}
+
+sumB (10, 10)
+sumB (20, 10)
+//so now returning the result to us
+
+
+func sumC(firstNum: Int, secondNum: Int) -> Int {
+    let newSum = firstNum + secondNum
+    return newSum
+}
+
+var x = sumC(10, 20)
+var y = sumC(x, 15)
+
+//you can call functions within functions! Huzzah!
+/*
+func sumC(firstNum: Int, secondNum: Int) -> Int {
+    let newSum = firstNum + secondNum
+    hello()
+    return newSum
+}
+
+var x = sumC(10, 20)
+var y = sumC(x, 15)
+*/
+
+
+
+//--------------Fibonacci function------------------
+
+
+func fib(place: Int) -> Int {
+    var fibNum = place, current = 0, next = 1, result = 0
+    for index in 0..<fibNum {
+        let tempVar = current
+        current = next
+        next = tempVar + current
+        result = tempVar
+    }
+    
+    return result
+}
+
+fib(6)
+fib(9)
+
+
+
+//can add external variable (fibPlace) to help us remember what the internal variable (place) is
+func fibB(fibPlace place: Int) -> Int {
+    var fibNum = place, current = 0, next = 1, result = 0
+    for index in 0..<fibNum {
+        let tempVar = current
+        current = next
+        next = tempVar + current
+        result = tempVar
+    }
+    
+    return result
+}
+
+fibB (fibPlace: 9)
+
+// better to have internal and external variables have same name--more clear, but not very dry, so could do #place (declares both)
+
+
+
+func fibC(place: Int?) -> Int {
+    var fibNum = place!, current = 0, next = 1, result = 0
+    for index in 0..<fibNum {
+        let tempVar = current
+        current = next
+        next = tempVar + current
+        result = tempVar
+    }
+    
+    return result
+}
+
+var q = 8
+fib (10)
+
+
+// great way to unwrap optionals
+var num: Int? = 5
+num!
+if let numValue = num {
+    println(numValue + 5)
+}
+//this is basically saying if num is a value (ie not nil), then do something
+//so if it's nil, it won't do anything
+
+//optionals are a way to deal with errors/crash, since you're dealing with data that doesn't exist
+//optionals say hey, no stress, if nothing gets returned, do x
+//let us fail safely
+
+num
+// "Some 5" means there's some value that is called 5--it doesn't know what it is, it's type, etc, just knows that it's not nil
+// once you unwrap it, it recognizes it as an Int again
+
+num!
+
+
+func save(name: String) -> (first: String, last: String) {
+    var lastName = "Ostrich"
+    return (name, lastName)
+}
+//tuple (pronounced "toople") has multiple values
+
+var myName = save("Johanna")
+myName.first
+myName.last
+
+//when functions return more than 1 thing, we can call just a single result with .XXXXX
+
+
+
+
+
+
