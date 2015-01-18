1abe6fa52abad9445314fd1e1c892ff294d22ff4
Johanna Ostrich <jhostrich@gmail.com>
2015-01-17 22:32:40 -0500

    classwork 1.14.15

--- Homework/Week 2/Lesson02/FirstViewController.swift
+++ Homework/Week 2/Lesson02/FirstViewController.swift
@@ -14,6 +14,6 @@ class FirstViewController: UIViewController {
     
     TODO two: Connect the ‘name’ and ‘age’ text boxes to this class. Hook up the button to a NEW function (in addition to the function previously defined). That function must look at the string entered in the text box and print out “Hello {name}, you are {age} years old!”
     TODO three: Hook up the button to a NEW function (in addition to the two above). Print “You can drink” below the above text if the user is above 21. If they are above 18, print “you can vote”. If they are above 16, print “You can drive”
-    TODO four: Hook up the button to a NEW function (in additino to the three above). Print “you can drive” if the user is above 16 but below 18. It should print “You can drive and vote” if the user is above 18 but below 21. If the user is above 21, it should print “you can drive, vote and drink (but not at the same time!”.
+    TODO four: Hook up the button to a NEW function (in addition to the three above). Print “you can drive” if the user is above 16 but below 18. It should print “You can drive and vote” if the user is above 18 but below 21. If the user is above 21, it should print “you can drive, vote and drink (but not at the same time!”.
     */
 }
