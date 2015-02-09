01625549f2916e690e83502f0c100e5b98f71001
Johanna Ostrich <jhostrich@gmail.com>
2015-02-09 14:46:02 -0500

    hw for 2.9.15

---
+++ Classwork/2.4.15_work/login/login/SecondViewController.swift
@@ -0,0 +1,57 @@
+//
+//  SecondViewController.swift
+//  login
+//
+//  Created by Johanna Ostrich on 2/9/15.
+//  Copyright (c) 2015 Johanna Ostrich. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // ----- TITLE LABEL -----
+        var titleLabel = UILabel(frame: CGRect(x: 50, y: 90, width: self.view.frame.width-100, height: 40))
+        titleLabel.text = "You logged in successfully!"
+        self.view.addSubview(titleLabel)
+        
+        
+        // ----- BOX SCROLL VIEW -----
+        var boxScrollView = UIScrollView(frame: CGRect(x: 50, y: 150, width: 200, height: 300))
+        boxScrollView.backgroundColor = UIColor.whiteColor()
+        boxScrollView.contentSize = CGSize(width: 200, height: 630)
+        self.view.addSubview(boxScrollView)
+        
+        
+        // ----- IMAGE 1 -----
+        var babyImg = UIImage(named: "baby.jpg")
+        var babyImgView = UIImageView(image: babyImg)
+        babyImgView.frame = CGRect(x: 0, y: 0, width: babyImg!.size.width, height: babyImg!.size.height)
+        
+        // ----- IMAGE 2 -----
+        var batmanImg = UIImage(named: "batman.jpg")
+        var batmanImgView = UIImageView(image: batmanImg)
+        batmanImgView.frame = CGRect(x: 0, y: 160, width:batmanImg!.size.width, height: batmanImg!.size.height)
+        
+        // ----- IMAGE 3 -----
+        var fonzImg = UIImage(named: "theFonz.jpg")
+        var fonzImgView = UIImageView(image: fonzImg)
+        fonzImgView.frame = CGRect(x: 0, y: 320, width: fonzImg!.size.width, height: fonzImg!.size.height)
+        
+        // ----- IMAGE 4 -----
+        var obamaImg = UIImage(named: "president.jpg")
+        var obamaImgView = UIImageView(image: obamaImg)
+        obamaImgView.frame = CGRect(x: 0, y: 480, width: obamaImg!.size.width, height: obamaImg!.size.height)
+        
+        boxScrollView.addSubview(babyImgView)
+        boxScrollView.addSubview(batmanImgView)
+        boxScrollView.addSubview(fonzImgView)
+        boxScrollView.addSubview(obamaImgView)
+
+    }
+
+
+}
---
+++ Homework/Week 5/Hangman/hangman/hangman/ViewController.swift
@@ -0,0 +1,83 @@
+//
+//  ViewController.swift
+//  hangman
+//
+//  Created by Johanna Ostrich on 2/9/15.
+//  Copyright (c) 2015 Johanna Ostrich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    /*
+    var wrongGuesses: [String] = []
+    var correctGuesses: [String] = []
+    var incompleteWord = ""
+    
+    if myWord.lowercaseString.rangeOfString("e") != nil {
+    
+    }
+    
+    
+    for letter in myWord {
+        if contains(correctGuesses, String(letter).lowercaseString) {
+            incompleteWord += String(letter)
+            incompleteWord += " "
+        } else {
+            incompleteWord += "_ "
+        }
+    }
+    
+    
+    */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
+/*
+
+
+Okay, so what are the steps I need to take?
+
+so I need to set the phrase, then set a correct guesses array
+so let's say my phrase is "Luke I am your father"
+var correctGuesses = ["l", "u", "k", "e", "i", "a", "m", "y", "o", "u", "r", "f", "t", "h"]
+
+use for loop to write the word out, make sure can get spaces between words in there
+
+add letter--> goes to next view
+
+in KeyboardViewController
+hit key, that key represents that letter--not sure how that will work--I think button.text will be the letter
+
+when hit key, several things happen:
+- the key changes--if correct, green, or loses background
+                    if incorrect, red
+- passes 2 things back--the letter, and if the letter is correct or incorrect
+- if incorrect, draws another part of hangman, plus # of wrong guesses label changes
+- if correct, letter is revealed
+
+- also needs to listen for gameOverGood and gameOverBad
+
+- when drawing hangman, need to pay attention to springs and struts, so this will need to be drawn programmatically
+
+
+
+so I think the first step is to set up ViewController
+- make sure that the revealing of letters is working properly
+- then can move onto KeyboardViewController
+
+will have to see if the keyboard idea will work--might be too much for now
+possible issues are:
+- the Keyboard will need to remember it's right/wrong settings--do I know how to do that?
+- might get tricky with so many buttons, but again, I think the button.text will work
+- will just have to see
+
+
+*/
