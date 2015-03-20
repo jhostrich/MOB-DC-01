5112ad1ffc9f648f97c2b2d924724002abf289da
Adam Ostrich <adam@Adams-MacBook-Air.local>
2015-03-20 02:00:49 -0400

    Finished up the basics for the search results page

--- Final/harvestDC/harvestDC/SearchResultsViewController.swift
+++ Final/harvestDC/harvestDC/SearchResultsViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import MapKit
+import NYSegmentedControl
 
 protocol SearchResultsDelegate {
     // Two types: "Vendors" or "Markets"
@@ -22,15 +24,36 @@ class SearchResultsViewController: UIViewController, UITableViewDataSource, UITa
     // UIView with filter button
     var subNavController: UIView!
     var filterBtn: UIButton!
-    var listMapControl: UISegmentedControl!
+    var listMapControl: NYSegmentedControl!
     
-    // Current Display Mode ("list" or "map")
-    // Default to "list"
-    var mode: String = "list"
-
+    // Table View
+    var tableView: UITableView?
+    var tableCell: UITableViewCell?
+    
+    // Map View
+    var mapView: MKMapView?
+    
+    // Mode: "Vendors" or "Markets"
+    var mode: String?
+    
+    // Search Results
+    var searchResults: [String] = ["Bob", "Harry", "Job"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+    
+        // Draw subNavController
+        // Includes Filter Button and List/Map Switch
+        drawSubNavController()
+        
+        // Draw Table View
+        drawTableView()
+    }
+    
+    
+    // Draw subNavController
+    func drawSubNavController() {
         // Setup the subNavController
         subNavController = UIView()
         subNavController.backgroundColor = UIColor.blackColor()
@@ -65,23 +88,85 @@ class SearchResultsViewController: UIViewController, UITableViewDataSource, UITa
         
         
         // listMapSwitch
-        listMapControl = UISegmentedControl(items: ["List", "Map"])
+        listMapControl = NYSegmentedControl(items: ["List", "Map"])
         listMapControl.selectedSegmentIndex = 0
-//        listMapControl.numberOfSegments = 2
+        listMapControl.borderWidth = 1.0
+        listMapControl.borderColor = UIColor(white: 0.15, alpha: 1.0)
+        listMapControl.cornerRadius = 15
+        listMapControl.backgroundColor = UIColor.blueColor()
+        listMapControl.titleTextColor = UIColor.blackColor()
+        listMapControl.selectedTitleTextColor = UIColor.whiteColor()
+        listMapControl.segmentIndicatorInset = 2.0
+        listMapControl.segmentIndicatorAnimationDuration = 0.3
+        listMapControl.segmentIndicatorBorderWidth = 0.0
+        listMapControl.sizeToFit()
         
-//        customSegmentedControl.frame = CGRectMake(60, 250,200, 30)
-//        customSegmentedControl.selectedSegmentIndex = 1
-//        customSegmentedControl.tintColor = UIColor.redColor()
-//        customSegmentedControl.addTarget(self, action: "segmentedValueChanged:", forControlEvents: .ValueChanged)
-
-        
-        
+        listMapControl.addTarget(self, action: "switchListMap", forControlEvents: UIControlEvents.ValueChanged)
         subNavController.addSubview(listMapControl)
         
         // listMapControl Constraints
         listMapControl.snp_makeConstraints { (make) -> Void in
             make.centerY.equalTo(self.subNavController.snp_centerY)
             make.right.equalTo(self.subNavController.snp_right).offset(-20)
+        }
+
+    }
+    
+    
+    // Draw Table View
+    func drawTableView() {
+        self.tableView = UITableView()
+        
+        self.view.addSubview(self.tableView!)
+        
+        // Constraints
+        self.tableView?.snp_makeConstraints { (make) -> Void in
+            make.top.equalTo(self.subNavController.snp_bottom)
+            make.bottom.equalTo(self.view.snp_bottom)
+            make.width.equalTo(self.view.snp_width)
+        }
+
+        // Setup delegates
+        self.tableView?.dataSource = self
+        self.tableView?.delegate = self
+        
+        // Register Cell
+        self.tableView?.registerClass(UITableViewCell.self, forCellReuseIdentifier: "resultsCell")
+    }
+    
+    // Draw Map View
+    func drawMapView() {
+        self.mapView = MKMapView()
+        self.view.addSubview(self.mapView!)
+
+        
+        // Constraints
+        self.mapView?.snp_makeConstraints { (make) -> Void in
+            make.top.equalTo(self.subNavController.snp_bottom)
+            make.bottom.equalTo(self.view.snp_bottom)
+            make.width.equalTo(self.view.snp_width)
+        }
+    }
+    
+    
+    //
+    func switchListMap() {
+        // List
+        if self.listMapControl.selectedSegmentIndex == 0 {
+            // Remove Map View
+            self.mapView?.removeFromSuperview()
+            
+            // Draw Table View
+            drawTableView()
+        }
+        
+        // Map
+        else {
+            // Remove Table View
+            self.tableView?.removeFromSuperview()
+                
+            // Draw Map View
+            drawMapView()
         }
     }
 
@@ -92,46 +177,36 @@ class SearchResultsViewController: UIViewController, UITableViewDataSource, UITa
     
     // Number of Sections
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 0
+        return self.searchResults.count
     }
     
     // Number of Rows in Section
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return 1
     }
     
     // Define How to Populate Cells
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("campusCell", forIndexPath: indexPath) as? UITableViewCell ?? UITableViewCell(style: .Subtitle, reuseIdentifier: "campusCell")
+        let cell = tableView.dequeueReusableCellWithIdentifier("resultsCell", forIndexPath: indexPath) as? UITableViewCell ?? UITableViewCell(style: .Subtitle, reuseIdentifier: "resultsCell")
+
         
         // Here's the meat
-//        cell.textLabel?.text = gaCampuses[indexPath.section][indexPath.row]
-//        
-//        cell.backgroundColor = UIColor.grayColor()
-//        cell.textLabel?.textColor = UIColor.whiteColor()
+        cell.textLabel?.text = searchResults[indexPath.section]
+
+        cell.backgroundColor = UIColor.grayColor()
+        cell.textLabel?.textColor = UIColor.whiteColor()
         
         return cell
     }
     // ---------------------------------------------------
     
+    func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 5.0
+    }
+    
     //on click
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        /*
-        var url: NSURL?
-        
-        if let jsonData = self.json {
-        if let newArticles = jsonData["new"] as? NSArray {
-        if let article = newArticles[indexPath.row] as? NSDictionary {
-        if let link = article["link"] as? NSString {
-        url = NSURL(string: link)
-        }
-        }
-        }
-        }
-        
-        performSegueWithIdentifier("web", sender: NSURLRequest(URL: url!))
-        */
-        performSegueWithIdentifier("web", sender: indexPath.row)
+        println("Clicked on the bitch \(searchResults[indexPath.section])")
     }
     
     
